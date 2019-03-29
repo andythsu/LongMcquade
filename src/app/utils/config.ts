2390b839d54b177b5f957807d0cc8792890eabc1
andythsu <f2280c@gmail.com>
2019-03-29 19:08:07 -0400

    prod

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-  // server: "https://longmcquade-backend.herokuapp.com",
-  server: "http://localhost:3000",
+  server: "https://longmcquade-backend.herokuapp.com",
+  // server: "http://localhost:3000",
   userApi: "/user",
   tutorApi: "/tutor",
   studentApi: "/student",
